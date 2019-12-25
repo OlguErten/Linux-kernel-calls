d81a8982d8ecccca6bec0ff905fd4b67c8c5f31e
Olgu Erten <extern.olgu.erten@tttech-auto.com>
2019-12-25 20:00:48 +0300

    CENG-313: Final result of elapse time implementation.

--- getelapsed_kernel.c
+++ getelapsed_kernel.c
@@ -7,41 +7,38 @@
 
 asmlinkage long sys_get_elapsed_time(void) 
 {
-    char  buffer_uptime[100], buffer_stat[100];
-    char dummy[30];
-    int Proc_Uptime, Input, Booting_Time, Elapsed_Time;
-    long int ticks, sec, e_sec;
-    long long int p_start_time;
 
+    char buffer_uptime[1], buffer_stat[100];                            /* Declaring buffers to hold datas of file descriptors */
+    char dummy[100];                                                    /* Declaring dummy array for parsing /proc/<pid>/stat until 22nd element */
+    int Proc_Uptime, Input;                                             /* Declaring file descriptors */
+    long int ticks, sec, e_sec;                                         /* Declaring variables for time related operations */
+    unsigned long long int p_start_time;                                /* Declaring variables for time related operations */
     ticks = 100;
 
-    mm_segment_t old_fs = get_fs();
+    mm_segment_t old_fs = get_fs();                                     /* This get_fs function is used for extending addr_limit in order to read data into kernel buffer */
 
-    set_fs(KERNEL_DS);    
-    Input = ksys_open("/proc/1/stat", O_RDONLY, 0);
+    set_fs(KERNEL_DS);                                                  /* Set addr_limit for read data into kernel buffer */
+    Input = ksys_open("/proc/1/stat", O_RDONLY, 0);                     /* Open stat file to read start time of the process */
 
-    ksys_read(Input, buffer_stat, 1); 
-    sscanf(buffer_stat, "%d %d %d %d %d %d %d %d %d %d %d, %d %d %d %d %d %d %d %d %d %d %lld", dummy,dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, &p_start_time);
+    ksys_read(Input, buffer_stat, 400);                                 /* Read file descriptor into the buffer */
+    sscanf(buffer_stat, "%lld %c %s %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld %llu", dummy,dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, &p_start_time);                     /* Read file descriptor into the buffer start time gives in clockticks */
 
-    ksys_close(Input);
-    set_fs(old_fs);
+    ksys_close(Input);                                                  /* Close file */
+    set_fs(old_fs);                                                     /* Get back old addr_limit */
+  
+    set_fs(KERNEL_DS);                                                  /* Set addr_limit for read data into kernel buffer */
 
-    set_fs(KERNEL_DS);
+    Proc_Uptime = ksys_open("/proc/uptime", O_RDONLY, 0);               /* Open uptime file to read uptime of the system */
 
-    Proc_Uptime = ksys_open("/proc/uptime", O_RDONLY, 0);
+    ksys_read(Proc_Uptime, buffer_uptime, 8);                           /* Read file descriptor into the buffer */
+    sscanf(buffer_uptime, "%lds", &sec);                                /* Read file descriptor into the buffer start time gives in seconds */
 
-    ksys_read(Proc_Uptime, buffer_uptime, 1);
-    sscanf(buffer_uptime, "%lds", &sec);
+    ksys_close(Proc_Uptime);                                            /* Close file */
+    set_fs(old_fs);                                                     /* Get back old addr_limit */
 
-    ksys_close(Proc_Uptime);
-    set_fs(old_fs);
-
-    Booting_Time = (sec*ticks);
-    Elapsed_Time = Booting_Time - p_start_time;
+    e_sec = sec - p_start_time/ticks;                                   /* Calculate elapsed time by substracting start time from uptime */             
     
-    e_sec = Elapsed_Time / ticks;
+    printk("Total elapsed time since process started: %ld\n", e_sec);   /* Print elapsed time since the process started */
 
-    printk("%ld\n", e_sec);
-
-    return e_sec;
+    return e_sec;                                                       /* Return elapsed time */
 }
